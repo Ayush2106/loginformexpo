efb9d2ec3cadb2091c308026de69df5bf86f5cc8
Ayush Singla <ayush21singla@gmail.com>
2022-09-15 15:54:30 +0530

    first

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={{width:150}}
+source={require('C:\Users\AYUSH\Desktop\Native\login\logo.jpg')}
+
+      />
+      {/* <StatusBar style="auto" /> */}
+      <Text>Hello g kkrh aaj kal </Text>
     </View>
   );
 }
